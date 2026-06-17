f891c1e136ff633fb71e751afef8789784d120a4
Yuki Nakakura <hiroaki.dev@gmail.com>
2026-06-17 22:41:31 +0900

    Add app icon asset

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -6,14 +6,16 @@ APP_NAME="SelectTranslate"
 APP_DIR="$ROOT_DIR/build/$APP_NAME.app"
 CONTENTS_DIR="$APP_DIR/Contents"
 MACOS_DIR="$CONTENTS_DIR/MacOS"
+RESOURCES_DIR="$CONTENTS_DIR/Resources"
 
 cd "$ROOT_DIR"
 swift build -c release
 
 rm -rf "$APP_DIR"
-mkdir -p "$MACOS_DIR"
+mkdir -p "$MACOS_DIR" "$RESOURCES_DIR"
 
 cp "$ROOT_DIR/.build/release/$APP_NAME" "$MACOS_DIR/$APP_NAME"
+cp "$ROOT_DIR/Resources/AppIcon.icns" "$RESOURCES_DIR/AppIcon.icns"
 
 cat > "$CONTENTS_DIR/Info.plist" <<'PLIST'
 <?xml version="1.0" encoding="UTF-8"?>
@@ -26,6 +28,8 @@ cat > "$CONTENTS_DIR/Info.plist" <<'PLIST'
   <string>SelectTranslate</string>
   <key>CFBundleIdentifier</key>
   <string>local.SelectTranslate</string>
+  <key>CFBundleIconFile</key>
+  <string>AppIcon</string>
   <key>CFBundleInfoDictionaryVersion</key>
   <string>6.0</string>
   <key>CFBundleName</key>
